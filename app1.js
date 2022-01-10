eaf27f76d557ec0d7b3b5b4e686e21e9dfcad8c2
Shaik Zabihulla <informshaikz@gmail.com>
2022-01-10 13:45:15 +0530

    easy-1 completed

---
+++ app1.js
@@ -0,0 +1,26 @@
+// Functions
+// Easy
+// Given a and b, your function should return the value of ab
+// Example:
+// Input: power(2,3) ––> Output: 8
+
+// const outputRes = document.querySelector("#output");
+
+// function Power(a,b) {
+//  let result = Math.pow(a,b);
+//  outputRes.innerText = result;
+
+// }
+
+// Power(2,3);
+
+//Using Arrow Function
+
+const outputRes = document.querySelector("#output");
+
+Power = (a,b) => {
+    let result = Math.pow(a,b);
+    // console.log(result);
+    outputRes.innerText = result;
+}
+Power(2,3);
